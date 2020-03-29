484cf55cfcbd766d381bc2063793cf5b42d7e5e1
Igor Shkidchenko <igorshkidchenko@gmail.com>
2020-03-29 15:16:30 +0300

    remove unused libs

--- src/components/HomePage/RepositoryForm/__tests__/container.test.js
+++ src/components/HomePage/RepositoryForm/__tests__/container.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { waitFor } from "@testing-library/dom";
 import configureStore from "redux-mock-store";
 
 import RepositoryForm from "../container";
@@ -12,22 +11,9 @@ describe("<RepotisoryRow />", () => {
   const store = mockStore(initialState);
 
   let container;
-  beforeEach(() => {
-    const wrapper = shallow(<RepositoryForm store={store} />);
-    container = wrapper.dive().dive().dive();
-  });
-
   it("renders correctly", () => {
-    expect(container).toMatchSnapshot();
-  });
-
-  it("should call onAddRepository on submit", async () => {
-    const value = { target: { name: "repositoryUrl", value: "facebook/react" } };
-    container.find("input").simulate("change", value);
-
-    await waitFor(() => {
-      container.find("form").simulate("submit");
-      expect(clickFn).toHaveBeenCalledTimes(1);
-    });
+    const wrapper = shallow(<RepositoryForm store={store} />);
+    container = wrapper;
+    expect(container.dive().dive().dive()).toMatchSnapshot();
   });
 });
